ebffc597d40a91b2c480852aceff3da63ad154c9
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2017-04-24 17:14:18 -0500

    Add state "E" to example code state machine

--- example_code.js
+++ example_code.js
@@ -6,7 +6,7 @@ function scratchLogicLoop(svm) {
 			switch(svm.reply()) {
 				case 'refund':
 					if (svm.currency() > 0) {
-						svm.state('E');
+						svm.state('D');
 					}
 					break;
 				default:
@@ -23,12 +23,14 @@ function scratchLogicLoop(svm) {
 
 			switch (svm.reply()) {
 				case 'refund':
-					svm.state('E');
+					svm.state('D');
 					break;
 				case 'soda':
 					var soda = svm.clicked();
 					if (soda && soda.count !== undefined) {
 						svm.slot(soda.index, soda.count - 1);
+						svm.dispense(soda.index);
+						svm.currency(svm.currency() - 1.25);
 						svm.state('C');
 					}
 					else {
@@ -38,27 +40,26 @@ function scratchLogicLoop(svm) {
 			}
 			break;
 		case 'C':
-			var soda = svm.clicked();
-			svm.dispense(soda.index);
-			svm.say('Dispensing ' + soda.name + '. Please remove soda from dispenser.');
-			svm.currency(svm.currency() - 1.25);
-			svm.state('D');
-			break;
-		case 'D':
+			svm.say('Dispensing soda. Please remove soda from dispenser.');
+
 			if (svm.reply() === 'dispense') {
 				svm.takeItem();
 
 				if (svm.currency() > 0) {
-					svm.refund(svm.currency());
-					svm.state('E');
+					svm.state('D');
 				}
 				else {
 					svm.state('A');
 				}
 			}
 			break;
+		case 'D':
+			var currency = svm.currency();
+			svm.say('Returning $' + currency.toFixed(2) + ' in change. Please remove change.');
+			svm.refund(currency);
+			svm.state('E');
+			break;
 		case 'E':
-			svm.say('Returning $' + svm.refund().toFixed(2) + ' in change. Please remove change.');
 			if (svm.reply() === 'cash') {
 				svm.say();
 				svm.takeRefund();
@@ -75,8 +76,8 @@ function createSVM() {
 	// Create new SVM object
 	svm = new ScratchVendingMachine(container)
 		.slot(0, 5, 'Cola', './cola.png', 1.25)
-		.slot(1, 4, 'Lemon Lime', './lemonlime.png', 1.25)
-		.slot(2, 9, 'Ginger Ale', 'gingerale.png', 1.25)
+		.slot(1, 9, 'Ginger Ale', 'gingerale.png', 1.25)
+		.slot(2, 4, 'Root Beer', './rootbeer.png', 1.25)
 		.loop(scratchLogicLoop) // Set logic loop
 		.state('A'); // Begin loop at state A
 }
